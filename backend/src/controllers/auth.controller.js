6944646744deb6acb41df5ec764983fc2f3e51ed
Danish Shaikh <danishshk0399@gmail.com>
2025-09-20 04:20:32 +0530

    Implement authentication routes for signup, login, and logout

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+// Signup Route
+export const signup = async (req, res) => {
+    res.send("SignUp Route");
+}
+
+// Login Route
+export const login = async (req, res) => {
+    res.send("Login Route");
+}
+
+// Logout Route
+export const logout = (req, res) => {
+    res.send("Logout Route");
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import "dotenv/config.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 5500;
 
-app.get("/", (req, res) => {
-    res.send("Hello World From Express!");
-});
-
+app.use("/api/auth/", authRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
