4bfd2684fcce04efe642a8a7db585c3d3342b81b
Danish Shaikh <danishshk0399@gmail.com>
2025-09-21 13:42:25 +0530

    Fix comment typo and add password modification check in preSave hook

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -58,11 +58,12 @@ const userSchema = new mongoose.Schema(
 
 const User = mongoose.model("User", userSchema);
 
-// create a pre hook to hash password before saving the user to the database using bcryptjs library
+// create a preSave hook to hash password before saving the user to the database using bcryptjs library
 // e.g. => password: 123456 -> $2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW
 // this method is called salting and hashing
 
 userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
